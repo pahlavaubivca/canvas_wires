a0903d1c059e3add18f6e6c4949f269175a0b68e
pahlavaubivca <pahlavaubivca@gmail.com>
2018-02-17 19:40:45 +0200

    Change params type, accept not only array but also number and string. Improve parseColor method - accept string|number|Array<number> Add helper function:  - distBetweenPoints  - radToDeg  - degToRag

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import {Painter} from "./lib/painter";
 interface IParams {
     width: number;
     height: number;
-    base?: IBase;
+    wires?: IWires;
     beam?: IBeam;
 }
 
@@ -16,18 +16,18 @@ export interface IPoints {
 }
 
 export interface IColor {
-    red: number[];
-    green: number[];
-    blue: number[];
-    opacity: number[];
+    red: number[] | number;
+    green: number[] | number;
+    blue: number[] | number;
+    opacity: number[] | number;
 }
 
-export interface IBase {
-    color: IColor[];
-    count: number[];
-    width: number[];
-    distX: number[];
-    distY: number[];
+export interface IWires {
+    color: IColor[] | string[];
+    count: number[] | number;
+    width: number[] | number;
+    distX: number[] | number;
+    distY: number[] | number;
 }
 
 export interface IBeam {
@@ -38,11 +38,11 @@ export interface IBeam {
     direction: number;
 }
 
-export class Tracer {
+export class Wires {
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
     private coordinate: { x: number, y: number }[];
-    private base: IBase;
+    private wires: IWires;
     private beam: IBeam;
     private drawer;
 
@@ -56,18 +56,17 @@ export class Tracer {
     public setCoordinate(coordinate: { x: number, y: number }[]) {
         this.coordinate = coordinate;
         this.drawer = new Painter(coordinate, this.ctx);
-
     }
 
     private parseParams(params: IParams) {
-        this.base = {
-            color: params.base && params.base.color || [{red: [250], green: [0], blue: [0], opacity: [0.5, 1]}],
-            // rangeX: params.base && params.base.rangeX || [30, -30],
-            // rangeY: params.base && params.base.rangeY || [0, 0],
-            count: params.base && params.base.count || [5, 8],
-            width: params.base && params.base.width || [0.2, 2],
-            distX: params.base && params.base.distX || [3, 5],
-            distY: params.base && params.base.distY || [3, 5]
+        this.wires = {
+            color: params.wires && params.wires.color || [{red: [250], green: [0], blue: [0], opacity: [0.5, 1]}],
+            // rangeX: params.wires && params.wires.rangeX || [30, -30],
+            // rangeY: params.wires && params.wires.rangeY || [0, 0],
+            count: params.wires && params.wires.count || [5, 8],
+            width: params.wires && params.wires.width || [0.2, 2],
+            distX: params.wires && params.wires.distX || [3, 5],
+            distY: params.wires && params.wires.distY || [3, 5]
         };
         this.beam = {
             color: params.beam && params.beam.color || [{red: [150], green: [50], blue: [0], opacity: [0.5, 1]}],
@@ -82,9 +81,13 @@ export class Tracer {
         container.appendChild(this.canvas);
     }
 
+    public getCanvas(): HTMLCanvasElement {
+        return this.canvas;
+    }
+
     public run() {
-        this.drawer.drawBase(this.base);
+        this.drawer.drawBase(this.wires);
     }
 }
 
-(window as any).Tracer = Tracer;
+(window as any).Tracer = Wires;
--- lib/painter.ts
+++ lib/painter.ts
@@ -1,28 +1,30 @@
-import {IBase, IBeam, IColor, IPoints} from "../index";
+import {IWires, IBeam, IColor, IPoints} from "../index";
 
 export class Painter {
     private arrPoints: { x: number, y: number }[];
     private ctx: CanvasRenderingContext2D;
-    private lines: IPoints[] = [];
+    private lines: IPoints[];
 
     constructor(points: { x: number, y: number }[], ctx: CanvasRenderingContext2D) {
         this.arrPoints = points;
         this.ctx = ctx;
     }
 
-    public drawBase(baseOption: IBase) {
+    public drawBase(b: IWires) {
+        this.lines = [];
+        const _amount = this.getAmount(b);
         this.lines.push({
             points: this.arrPoints,
-            width: random(baseOption.width[0], baseOption.width[1]),
-            color: this.parseColor(baseOption.color)
+            width: this.getWidth(b),
+            color: this.parseColor(b.color)
         });
-        const _count = random(baseOption.count[0], baseOption.count[1]);
+
         let oldSX = 0, oldSY = 0;
-        const minDX = baseOption.distX[0];
-        const maxDX = baseOption.distX[1];
-        const minDY = baseOption.distY[0];
-        const maxDY = baseOption.distY[1];
-        for (let i = 0; i < _count; i++) {
+        const minDX = b.distX[0];
+        const maxDX = b.distX[1];
+        const minDY = b.distY[0];
+        const maxDY = b.distY[1];
+        for (let i = 0; i < _amount; i++) {
             let sX = random(minDX, maxDX);
             let sY = random(minDY, maxDY);
             sX = oldSX = sX + oldSX;
@@ -35,15 +37,24 @@ export class Painter {
                     y: value.y + sY
                 })
             });
-            const width = random(baseOption.width[0], baseOption.width[1]);
-            const color = this.parseColor(baseOption.color);
+            const color = this.parseColor(b.color);
             this.lines.push({
                 points: points,
-                width: width,
+                width: this.getWidth(b),
                 color: color
             })
         }
         this.drawLines();
+    }
+    private getPoints(){
+
+    }
+    private getWidth(b: IWires): number {
+        return typeof b.width === "number" ? b.width : random(b.width[0], b.width[1]);
+    }
+
+    private getAmount(b: IWires): number {
+        return typeof b.count === "number" ? b.count : random(b.count[0], b.count[1]);
     }
 
     private drawLines() {
@@ -53,38 +64,51 @@ export class Painter {
             this.ctx.strokeStyle = value.color;
             value.points.forEach((point, i) => {
                 if (i === 0) {
-                    this.moveTo(point.x, point.y);
+                    this.ctx.moveTo(point.x, point.y);
                 }
                 if (i < value.points.length - 1) {
                     const next = value.points[i + 1];
-                    this.drawRange(point.x, point.y, next.x, next.y);
+                    this.drawLine(point.x, point.y, next.x, next.y);
                 }
             });
         });
     }
 
 
-    private drawRange(x0: number, y0: number, x1: number, y1: number) {
+    private drawLine(x0: number, y0: number, x1: number, y1: number) {
         this.ctx.lineTo(x0, y0);
         this.ctx.lineTo(x1, y1);
         this.ctx.stroke();
     }
 
-    private moveTo(x: number, y: number) {
-        this.ctx.moveTo(x, y);
-    }
-
-    private parseColor(colors: IColor[]): string {
-        const cc = Math.floor(random(0, colors.length - 0.1));
-        const currentColor = colors[cc];
-        const red = Math.floor(random(currentColor.red[0], currentColor.red[1]));
-        const green = Math.floor(random(currentColor.green[0], currentColor.green[1]));
-        const blue = Math.floor(random(currentColor.blue[0], currentColor.blue[1]));
-        const opacity = random(currentColor.opacity[0], currentColor.opacity[1]);
-        return `rgba(${red},${green},${blue},${opacity})`;
+    private parseColor(colors: Array<IColor | string>): string {
+        let color;
+        let cColor = colors[Math.floor(random(0, colors.length - 0.1))];
+        if (typeof colors === "string") {
+            color = cColor
+        } else {
+            cColor = cColor as IColor;
+            const red = typeof cColor.red === "number" ? cColor.red : Math.floor(random(cColor.red[0], cColor.red[1]));
+            const green = typeof cColor.green === "number" ? cColor.green : Math.floor(random(cColor.green[0], cColor.green[1]));
+            const blue = typeof cColor.blue === "number" ? cColor.blue : Math.floor(random(cColor.blue[0], cColor.blue[1]));
+            const opacity = typeof cColor.opacity === "number" ? cColor.opacity : random(cColor.opacity[0], cColor.opacity[1]);
+            color = `rgba(${red},${green},${blue},${opacity})`;
+        }
+        return color;
     }
 }
 
+const degToRad = (deg) => {
+    return (Math.PI / 180) * deg;
+};
+
+const radToDeg = (rad) => {
+    return rad * 180 / Math.PI;
+};
+
+const distBetweenPoints = (x0, y0, x1, y1) => {
+    return Math.sqrt(Math.pow((x1 - x0), 2) + Math.pow((y1 - y0), 2));
+};
 const random = (min, max) => {
     return Math.random() * (max - min) + min;
 };
