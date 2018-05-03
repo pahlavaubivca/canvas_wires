f98c967173b6e4caa8b8c61d422b43394d5821c8
pahlavaubivca <pahlavaubivca@gmail.com>
2018-05-03 23:25:04 +0300

    wires generator  - shift x and y relative 0  - add comment

--- lib/wire/wires_generator.ts
+++ lib/wire/wires_generator.ts
@@ -4,19 +4,28 @@ import {getAmount, random} from "../helper";
 
 export class WiresGenerator {
     private opt: IWires;
-    private buffCoord: Array<{ x: number, y: number }> = [];
+    /**
+     * @desc Push first coordinate, considering this be to take place next coordinate shift
+     * */
+    private bufferShiftsXY: Array<{ x: number, y: number }> = [];
+    /**
+     * @desc Switch next coordinate to opposite direction relative 0
+     * */
     private trigger: number = 1;
     private basePoint: Point;
 
     constructor(basePoint: Point, options: IWires) {
         this.opt = options;
         this.basePoint = basePoint;
-        this.buffCoord.push({x: basePoint.x, y: basePoint.y});
+        this.bufferShiftsXY.push({x: 0, y: 0});
         const amount = getAmount(this.opt.amount);
         for (let i = 0; i < amount; i++) this.genNextCoord();
     }
 
-    // TODO redo method. this method must return diff value for x & y
+    /**
+     * @description Determine shift for x and y coordinate, relative base point root coordinate,
+     * considering option for base wires.
+     * */
     private genNextCoord() {
         const delta = {
             x: Math.round(random(this.opt.distanceRange.x[0], this.opt.distanceRange.x[1])),
@@ -24,9 +33,17 @@ export class WiresGenerator {
         };
         const rand = random(-1, 1) >= 0 ? 1 : -1;
         const res = {x: 0, y: 0};
+        /**
+         * @desc first and second variable take x|y string name for determine
+         * who next have shift relative own parent coordinate,
+         * and who just take random coordinate from range option
+         * */
         const first = !this.opt.splitBy ? "x" : "y";
         const second = this.opt.splitBy ? "x" : "y";
-        res[first] = this.buffCoord.reduce((a, c) => {
+        /**
+         * @desc Get min|max from x|y coordinate for determine parent coordinate
+         * */
+        res[first] = this.bufferShiftsXY.reduce((a, c) => {
             if (this.trigger > 0) {
                 return a[first] < c[first] ? c : a;
             } else {
@@ -35,12 +52,16 @@ export class WiresGenerator {
         })[first] + delta[first] * this.trigger;
         res[second] = delta[second] * rand;
         this.trigger *= -1;
-        this.buffCoord.push({x: res.x, y: res.y});
+        this.bufferShiftsXY.push({x: res.x, y: res.y});
     }
 
+    /**
+     * @description First create from bufferShiftsXY coordinates for line,
+     * and next create instance of Point class.
+     * */
     public getArrayPoints(): Array<Point> {
         const arrayLines = [];
-        this.buffCoord.forEach((v, k) => {
+        this.bufferShiftsXY.forEach((v, k) => {
             if (k > 0) {
                 const coord = this.generateNewCoord(this.basePoint, v.x, v.y);
                 arrayLines.push(new Point(coord, null));
@@ -49,10 +70,13 @@ export class WiresGenerator {
         return arrayLines;
     }
 
+    /**
+     * @description This method return coordinate for next line with shift from bufferShiftsXY.
+     * */
     private generateNewCoord(point: Point, x: number, y: number): ICoordinate {
         const newCoordinate: ICoordinate = {
-            x: x > 0 ? (point.x + (this.basePoint.x - x)) : point.x,
-            y: y > 0 ? (point.y + (this.basePoint.y - y)) : point.y
+            x: point.x + x,
+            y: point.y + y
         };
         if (point.childs) {
             newCoordinate.childs = [];
